25a71f78ebeedc0525f745779b77df488a21a48e
wishtoday238 <wishtoday238@gmail.com>
2025-07-11 02:16:57 +0800

    更新1.21.6

--- src/main/java/com/wishtoday/er/mixin/FoodComponentsMixin.java
+++ src/main/java/com/wishtoday/er/mixin/FoodComponentsMixin.java
@@ -17,10 +17,6 @@ public class FoodComponentsMixin {
     public static FoodComponent ENCHANTED_GOLDEN_APPLE = new FoodComponent.Builder()
             .nutrition(6)
             .saturationModifier(1.2F)
-            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 1200, 4), 1.0F)
-            .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 6000, 0), 1.0F)
-            .statusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 6000, 0), 1.0F)
-            .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 2400, 3), 1.0F)
             .alwaysEdible()
             .build();
 }
